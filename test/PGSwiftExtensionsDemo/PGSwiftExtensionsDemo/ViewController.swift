61692e3bd7977345a87b6b39bb2932ce612dbd31
renxun <490282258@qq.com>
2018-11-12 18:54:10 +0800

    UIButton拓展 添加点击事件

--- test/PGSwiftExtensionsDemo/PGSwiftExtensionsDemo/ViewController.swift
+++ test/PGSwiftExtensionsDemo/PGSwiftExtensionsDemo/ViewController.swift
@@ -16,15 +16,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let string1 = """
-        asdliqjwd
-        qweoijqowe qweuhqiuwheqhwe
-asdqwdqwd
-
-"""
+//        let string1 = """
+//        asdliqjwd
+//        qweoijqowe qweuhqiuwheqhwe
+//asdqwdqwd
+//
+//"""
+//        print(self.className)
 //       var height = string1.height(kScreenWidth - 32, font: UIFont.systemFont(ofSize: 14), lineBreakMode: NSLineBreakMode.byWordWrapping)
 //        print(height)
-    
+        
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        view.addSubview(button)
+        button.frame = CGRect(x: 0, y: 100, w: 100, h: 100)
+        button.backgroundColor = UIColor.red
+        
+        button.addAction { (sender) in
+            print("click")
+        }
+//        button.addAction(buttonBlock: { (maker) in
+//            print("click")
+//        }, for: UIControl.Event.touchUpInside)
     }
     
     
