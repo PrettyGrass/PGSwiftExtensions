0297dad94f7f5060f8fefcb7842b3e6917595fea
renxun <renxun@doupai.cc>
2019-01-03 18:32:15 +0800

    UITextField 设置 placeHolder拓展

--- src/UITextFieldExtensions.swift
+++ src/UITextFieldExtensions.swift
@@ -12,6 +12,15 @@
 import UIKit
 
 extension UITextField {
+    
+    public func setPlaceHolder(text: String, textColor: UIColor, font: UIFont) {
+        let placeholserAttributes = [NSAttributedString.Key.foregroundColor : textColor ,NSAttributedString.Key.font : font]
+        self.attributedPlaceholder = NSAttributedString(string: text,attributes: placeholserAttributes)
+    }
+}
+
+
+extension UITextField {
     /// EZSE: Regular exp for email
     static let emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
     
