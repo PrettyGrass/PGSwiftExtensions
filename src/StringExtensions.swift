5053d25a155ae654cb9c83f503212e5f124765d9
renxun <renxun@doupai.cc>
2019-03-04 14:52:47 +0800

    String拓展 计算一个路径下(文件,文件夹)的size

--- src/StringExtensions.swift
+++ src/StringExtensions.swift
@@ -150,6 +150,44 @@ extension String {
     }
 }
 
+//MARK: - 关于文件操作
+extension String {
+    // 对象方法
+   public func getFileSize() -> UInt64  {
+        var size: UInt64 = 0
+        let fileManager = FileManager.default
+        var isDir: ObjCBool = false
+        let isExists = fileManager.fileExists(atPath: self, isDirectory: &isDir)
+        // 判断文件存在
+        if isExists {
+            // 是否为文件夹
+            if isDir.boolValue {
+                // 迭代器 存放文件夹下的所有文件名
+                let enumerator = fileManager.enumerator(atPath: self)
+                for subPath in enumerator! {
+                    // 获得全路径
+                    let fullPath = self.appending("/\(subPath)")
+                    do {
+                        let attr = try fileManager.attributesOfItem(atPath: fullPath)
+                        size += attr[FileAttributeKey.size] as! UInt64
+                    } catch  {
+                        print("error :\(error)")
+                    }
+                }
+            } else {    // 单文件
+                do {
+                    let attr = try fileManager.attributesOfItem(atPath: self)
+                    size += attr[FileAttributeKey.size] as! UInt64
+                    
+                } catch  {
+                    print("error :\(error)")
+                }
+            }
+        }
+        return size
+    }
+}
+
 extension String {
     
     public func isNewer(than aVersionString: String) -> Bool {
